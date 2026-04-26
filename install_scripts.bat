ad49cdb2821ea520a0d5de99da1bab289e43e8c7
ohnishi-med <coin.or.hot.dish@gmail.com>
2026-04-27 08:14:46 +0900

    feat: add salt-intake-calculator and hasegawa-hdrs-integration to install scripts

--- install_scripts.bat
+++ install_scripts.bat
@@ -6,35 +6,43 @@ echo.
 echo * NOTE: If you have old Japanese-named scripts installed,
 echo * please delete them from Tampermonkey dashboard first!
 echo.
-echo Ready to install 4 new scripts.
+echo Ready to install 6 new scripts.
 pause
 
 echo.
-echo Opening 4 browser tabs...
+echo Opening 6 browser tabs...
 echo Please click the "Install" button on each tab.
 echo.
 timeout /t 2 /nobreak > nul
 
-echo 1/4: Opening cell-colorizer...
+echo 1/6: Opening cell-colorizer...
 start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/cell-colorizer.user.js
 timeout /t 1 /nobreak > nul
 
-echo 2/4: Opening bml-submit-detector...
+echo 2/6: Opening bml-submit-detector...
 start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/bml-submit-detector.user.js
 timeout /t 1 /nobreak > nul
 
-echo 3/4: Opening drug-master-manager...
+echo 3/6: Opening drug-master-manager...
 start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/drug-master-manager.user.js
 timeout /t 1 /nobreak > nul
 
-echo 4/4: Opening date-insulin-calc...
+echo 4/6: Opening date-insulin-calc...
 start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/date-insulin-calc.user.js
+timeout /t 1 /nobreak > nul
+
+echo 5/6: Opening salt-intake-calculator...
+start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/salt-intake-calculator.user.js
+timeout /t 1 /nobreak > nul
+
+echo 6/6: Opening hasegawa-hdrs-integration...
+start https://raw.githubusercontent.com/ohnishi-med/m3degikar_modifier/main/js/hasegawa-hdrs-integration.user.js
 timeout /t 1 /nobreak > nul
 
 echo.
 echo ==============================================
-echo All 4 tabs have been opened!
-echo Once you clicked "Install" on all 4 tabs in your browser,
+echo All 6 tabs have been opened!
+echo Once you clicked "Install" on all 6 tabs in your browser,
 echo you can close this black window.
 echo ==============================================
 pause
